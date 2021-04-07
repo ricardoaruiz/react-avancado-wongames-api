a728b62ddccaf34a7d79875f71e18764131a1c87
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2021-04-07 20:25:16 -0300

    Fazendo o scrapper de uma pagina de jogo do GOG com o JSDOM

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -7,6 +7,24 @@
 
 const axios = require('axios');
 
+/**
+ * Get Game info from GOG page game detail
+ * @param {*} slug
+ */
+const getGameInfo = async (slug) => {
+  const jsdom = require('jsdom');
+  const { JSDOM } = jsdom;
+  const body = await axios.get(`https://www.gog.com/game/${slug}`);
+  const dom = new JSDOM(body.data);
+  const description = dom.window.document.querySelector('.description')
+
+  return {
+    rating: 'BR0',
+    short_description: description.textContent.slice(0, 160),
+    description: description.innerHTML
+  }
+}
+
 module.exports = {
   populate: async (params) => {
     console.log('Chamando o serviço pouplate...')
@@ -14,7 +32,6 @@ module.exports = {
     const url = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`
     const {data: {products}} = await axios.get(url);
 
-
-    console.log('products ==>', products[0])
+    console.log('product ==>', await getGameInfo(products[0].slug))
   }
 };
